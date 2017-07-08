0c00175e10ece14e2f02c1f484d7285474037f43
safe <safe@SAFE-PC>
2017-07-08 19:53:21 +0300

    Add project files.

---
+++ test4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int s = 10;
+            long m = s;
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
